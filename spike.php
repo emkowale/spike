f74dec4c6e3c1ebf47191cd7053b6faf28354c16
emkowale <emkowale@gmail.com>
2025-09-05 18:51:41 -0400

    Release 1.2.2: bundle Dompdf vendor for hosted installs

--- spike.php
+++ spike.php
@@ -3,12 +3,12 @@
  * Plugin Name: Spike
  * Description: PHI Intake (3-step form with pain diagram, signature, PDF, CSV export, reCAPTCHA v3).
  * Author: Eric Kowalewski
- * Version: 1.2.1
+ * Version: 1.2.2
  * Update URI: https://github.com/emkowale/spike
  * Last Updated: 2025-08-29 17:20 EDT
  */
 if (!defined('ABSPATH')) exit;
-define('SPIKE_VER','1.2.1');
+define('SPIKE_VER','1.2.2');
 define('SPIKE_DIR', plugin_dir_path(__FILE__));
 define('SPIKE_URL', plugin_dir_url(__FILE__));
 if (file_exists(SPIKE_DIR.'vendor/autoload.php')) require SPIKE_DIR.'vendor/autoload.php';
